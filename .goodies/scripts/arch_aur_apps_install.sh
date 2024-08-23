452cb646275ff5f55e82cfc781fe56f9b7fa7e24
Luna <luna@luna.localdomain>
2024-08-23 09:08:53 +0100

    scripts

---
+++ .goodies/scripts/arch_aur_apps_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+packages=""
+add_package () {
+	packages+=$1
+	packages+=" "
+}
+install_packages () {
+	sudo pacman -S $packages
+}
+
+aur_packages=""
+add_aur_package () {
+	aur_packages+=$1
+	aur_packages+=" "
+}
+install_aur_packages () {
+	yay -S --noconfirm --needed $aur_packages
+}
+
+
+add_package "qt6-base" # qt6 and tools are needed for qimgv
+add_package "qt6-tools" # if this errors try with qt5
+add_aur_package "qimgv-git"
+
+install_packages
+install_aur_packages
