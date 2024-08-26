c91e0bbf4fc6a470450b2403f57deec1aec18f9e
Luna <rokdoombeast@gmail.com>
2024-08-26 10:01:41 +0100

    hyprland v1.1

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -7,7 +7,7 @@ return {
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
 
-		local file_name = "boykisser.lua"
+		local file_name = "eyes.lua"
 		local art = dofile(vim.fn.stdpath("config") .. "/lua/plugins/alpha_art/" .. file_name)
 
 		art.draw(dashboard)
