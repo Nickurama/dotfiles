18f8a985952bab5360a124570572f095b96f5fe0
Luna <rokdoombeast@gmail.com>
2024-10-13 15:06:51 +0100

    updated hyprland and others

--- .goodies/scripts/startup_desktop.sh
+++ .goodies/scripts/startup_desktop.sh
@@ -5,10 +5,10 @@ xrandr --output DP-1 --primary
 
 
 # mount disks
-sudo ntfs-3g /dev/sda1 /media/hdd1
+#sudo ntfs-3g /dev/sda1 /media/hdd1
 
-sudo ntfs-3g /dev/sdb2 /media/hdd2
-sudo mount /dev/sdb3 /media/hdd2_ext4
+#sudo ntfs-3g /dev/sdb2 /media/hdd2
+#sudo mount /dev/sdb3 /media/hdd2_ext4
 
-sudo ntfs-3g /dev/sdc4 /media/windows
-sudo mount /dev/sdc5 /media/ubuntu
+#sudo ntfs-3g /dev/sdc4 /media/windows
+#sudo mount /dev/sdc5 /media/ubuntu
